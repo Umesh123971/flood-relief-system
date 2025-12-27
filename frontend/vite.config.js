0dbedbe63edc78edd9fd618c6e2a12a6f4f3bb36
Umesh Gimhan Kannangara <umeshgimhan723@gmail.com>
2025-12-27 09:45:37 +0530

    changes in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,  
-    host: true,   // Optional: Allows access from network
-    historyApiFallback: true
-  }
+  plugins: [react()]
 })
